f05f48ed10787fc8b4008f5d81d871780decbc7e
Antfather <925627037@qq.com>
2018-06-11 00:50:27 +0800

    second commit

---
+++ js/51rgb.js
@@ -0,0 +1,9 @@
+$(".banner-box").slide({
+    titCell: ".banner-hd ul li",
+    mainCell: ".banner-bd",
+    autoPlay: 'true',
+    depayTime: 2500,
+    effect: 'leftLoop',
+    easing:'easeInBack',
+    trigger:'click'
+});
