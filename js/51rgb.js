ae82e493c665009f76969ac13d0d9bfec8a9db9c
Antfather <925627037@qq.com>
2018-06-16 00:41:26 +0800

    end

--- js/51rgb.js
+++ js/51rgb.js
@@ -19,7 +19,9 @@ $(function () {
     
     //登入
     $('.login-in').click(function(){
-        $('.registerbox').removeClass('hidden');
+         $('.registerbox').removeClass('hidden');
+         $('.sign-in').css('color','#188eee');
+         $('.sign-up').css('color','#333');
     });
     $('.icon-guanbi,.shade-box').click(function(){
         $('.registerbox').addClass('hidden');
@@ -27,24 +29,31 @@ $(function () {
     $('.login-up').click(function(){
          $('.registerbox,.signup-box').removeClass('hidden');
          $('.signin-box').addClass('hidden');
+         $('.sign-up').css('color','#188eee');
+         $('.sign-in').css('color','#333');
         
     });
     $('.sign-up').click(function(){
-        $('.signup-box').removeClass('hidden');
-        $('.signin-box,.phone-signup').addClass('hidden');
+         $('.signup-box').removeClass('hidden');
+         $('.signin-box,.phone-signup').addClass('hidden');
+         $('.sign-up').css('color','#188eee');
+         $('.sign-in').css('color','#333');
     });
     $('.sign-in').click(function(){
          $('.signin-box,').removeClass('hidden');
          $('.signup-box,.phone-signup').addClass('hidden');
+         $('.sign-in').css('color','#188eee');
+         $('.sign-up').css('color','#333');
     });
     $('.phone-signup-button').click(function(){
-        $('.phone-signup').removeClass('hidden');
-        $('.signup-box,.signin-box').addClass('hidden');
-        $('.inputbutton a').html('注 册')
+         $('.phone-signup').removeClass('hidden');
+         $('.signup-box,.signin-box').addClass('hidden');
+         $('.inputbutton a').html('注 册')
+
     });
      $('.usersign,.phone-sign').click(function(){
-        $('.phone-signup').removeClass('hidden');
-        $('.signup-box,.signin-box').addClass('hidden');
-        $('.inputbutton a').html('登 录')
+         $('.phone-signup').removeClass('hidden');
+         $('.signup-box,.signin-box').addClass('hidden');
+         $('.inputbutton a').html('登 录')     
     });
 });
