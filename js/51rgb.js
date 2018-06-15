c24d2f3fadb332ae56aa45af1dc3bafb99448351
Antfather <925627037@qq.com>
2018-06-16 00:24:39 +0800

    end

--- js/51rgb.js
+++ js/51rgb.js
@@ -1,25 +1,49 @@
-$(".banner-box").slide({
-    titCell: ".banner-hd ul li",
-    mainCell: ".banner-bd",
-    autoPlay: 'true',
-    depayTime: 2500,
-    effect: 'leftLoop',
-    easing:'easeInBack',
-    trigger:'click'
+
+$(function () {
+
+    //banner
+    $(".banner-box").slide({
+        titCell: ".banner-hd ul li",
+        mainCell: ".banner-bd",
+        autoPlay: 'true',
+        depayTime: 2500,
+        effect: 'leftLoop',
+        easing: 'easeInBack',
+        trigger: 'click'
+    });
+
+//    $(".login-in").on("click", function () {
+//        $(".shade-box").removeClass("hidden");
+//    });
+//
+    
+    //登入
+    $('.login-in').click(function(){
+        $('.registerbox').removeClass('hidden');
+    });
+    $('.icon-guanbi,.shade-box').click(function(){
+        $('.registerbox').addClass('hidden');
+    });
+    $('.login-up').click(function(){
+         $('.registerbox,.signup-box').removeClass('hidden');
+         $('.signin-box').addClass('hidden');
+        
+    });
+    $('.sign-up').click(function(){
+        $('.signup-box').removeClass('hidden');
+        $('.signin-box,.phone-signup').addClass('hidden');
+    });
+    $('.sign-in').click(function(){
+         $('.signin-box,').removeClass('hidden');
+         $('.signup-box,.phone-signup').addClass('hidden');
+    });
+    $('.phone-signup-button').click(function(){
+        $('.phone-signup').removeClass('hidden');
+        $('.signup-box,.signin-box').addClass('hidden');
+    });
+     $('.usersign,.phone-sign').click(function(){
+        $('.phone-signup').removeClass('hidden');
+        $('.signup-box,.signin-box').addClass('hidden');
+         $('.inputbutton a').html('登 录')
+    });
 });
-
-//$('.login-in') .onclick(function({
-//    
-//    $('signbox').removeClass('.hidden');
-//}));
-
-//$(".login-in").click(function(){
-//    $(".hidden").css("display","block");
-//   
-//});
-
-//$(".login-in").onmousemove(function(){
-//    $(".signbox").addClass("show");
-//   
-//});
-
